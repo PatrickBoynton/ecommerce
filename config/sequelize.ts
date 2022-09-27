aa8e3b8b75705a4a891839b933632002c23e86c7
Patrick Boynton <jpboynton3@gmail.com>
2022-09-27 13:51:45 -0400

    Added a connection file.

--- config/sequelize.ts
+++ config/sequelize.ts
@@ -1,11 +1,6 @@
 import {Sequelize} from "sequelize-typescript"
-import path from "path"
 
-const dotenv = require("dotenv")
-
-dotenv.config({path: path.resolve(__dirname, "./.env")})
-
-const sequelize = new Sequelize(process.env.DB!, process.env.USER!, process.env.PASSWORD, {
+const sequelize = new Sequelize("playground", "root", "safepass1", {
     dialect: "mysql"
 })
 
