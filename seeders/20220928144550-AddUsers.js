1530f71b74c5b0106d08d4c91c89ec8e36c3b5c6
Patrick Boynton <jpboynton3@gmail.com>
2022-09-28 13:37:02 -0400

    Set up and ran a migration.

---
+++ seeders/20220928144550-AddUsers.js
@@ -0,0 +1,40 @@
+'use strict';
+const bcrypt = require("bcryptjs")
+
+const users = [
+  {
+    id: 1,
+    name: "Admin User",
+    email:"admin@example.com",
+    password:bcrypt.hashSync("safepass1", 10),
+    isAdmin:true,
+  },
+  {
+    id: 2,
+    name: "Patrick",
+    email: "p@p.com",
+    password:bcrypt.hashSync("safepass1", 10),
+  },
+  {
+    id: 3,
+    name: "Jane",
+    email: "j@j.com",
+    password:bcrypt.hashSync("safepass1", 10),
+  },
+  {
+    id: 4,
+    name: "John",
+    email: "jo@jo.com",
+    password:bcrypt.hashSync("safepass1", 10),
+  },
+]
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return await queryInterface.bulkInsert("users", users)
+  },
+
+  async down (queryInterface, Sequelize) {
+    return await queryInterface.bulkDelete("users", users)
+  }
+};
