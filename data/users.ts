fae81fac2da4dc039997a70479ac81e66e4eacac
Patrick Boynton <jpboynton3@gmail.com>
2022-09-28 13:35:58 -0400

    Added product data for seeding. Slight refactor.

--- data/users.ts
+++ data/users.ts
@@ -1,27 +1,10 @@
 import bcrypt from "bcryptjs"
 
 const users = [
-	{
-	    name: "Admin User",
-	    email:"admin@example.com",
-		  password:bcrypt.hashSync("safepass1", 10),
-		  isAdmin:true,
-	},
-	{
-	    name: "Patrick",
-	    email: "p@p.com",
-		 password:bcrypt.hashSync("safepass1", 10),
-	},
-	{
-	    name: "Jane",
-	    email: "j@j.com",
-		  password:bcrypt.hashSync("safepass1", 10),
-	},
-	{
-	    name: "John",
-	    email: "jo@jo.com",
-		 password:bcrypt.hashSync("safepass1", 10),
-	},
+	{id: 1, name: "Admin User", email:"admin@example.com", password:bcrypt.hashSync("safepass1", 10), isAdmin:true,},
+	{id: 2, name: "Patrick", email: "p@p.com", password:bcrypt.hashSync("safepass1", 10),},
+	{id: 3, name: "Jane", email: "j@j.com", password:bcrypt.hashSync("safepass1", 10),},
+	{id: 4, name: "John", email: "jo@jo.com", password:bcrypt.hashSync("safepass1", 10),},
 ]
 
 export default users
