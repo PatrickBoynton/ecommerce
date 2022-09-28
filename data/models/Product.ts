b93b1205eb6f4d933b86fd0c17927b3aef2e8984
Patrick Boynton <jpboynton3@gmail.com>
2022-09-28 13:28:39 -0400

    Added a createdAt and updatedAt default values for migrations.

--- data/models/Product.ts
+++ data/models/Product.ts
@@ -1,47 +1,55 @@
-import sequelize from "../../config/sequelize";
-import {DataTypes} from "sequelize";
+import sequelize from "../../config/sequelize"
+import {DataTypes} from "sequelize"
 
 const Product = sequelize.define("products", {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    image: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    brand: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    category: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    description: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    rating: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 0
-    },
-    numReviews: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 0
-    },
-    price: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 0,
-    },
-    countInStock: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 0,
-    },
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false
+	},
+	image: {
+		type: DataTypes.STRING,
+		allowNull: false
+	},
+	brand: {
+		type: DataTypes.STRING,
+		allowNull: false
+	},
+	category: {
+		type: DataTypes.STRING,
+		allowNull: false
+	},
+	description: {
+		type: DataTypes.STRING,
+		allowNull: false
+	},
+	rating: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		defaultValue: 0
+	},
+	numReviews: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		defaultValue: 0
+	},
+	price: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		defaultValue: 0,
+	},
+	countInStock: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		defaultValue: 0,
+	},
+	createdAt: {
+		type: DataTypes.DATE,
+		defaultValue: DataTypes.NOW
+	},
+	updatedAt: {
+		type: DataTypes.DATE,
+		defaultValue: DataTypes.NOW
+	}
 })
 
 export default Product
--- data/models/User.ts
+++ data/models/User.ts
@@ -1,20 +1,32 @@
-import sequelize from "../../config/sequelize";
+import sequelize from "../../config/sequelize"
 import {DataTypes} from "sequelize"
 
 const User = sequelize.define("users", {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
-    isAdmin: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-    }
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false
+	},
+	email: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		unique: true
+	},
+	password: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	isAdmin: {
+		type: DataTypes.BOOLEAN,
+		defaultValue: false
+	},
+	createdAt: {
+		type: DataTypes.DATE,
+		defaultValue: DataTypes.NOW
+	},
+	updatedAt: {
+		type: DataTypes.DATE,
+		defaultValue: DataTypes.NOW
+	}
 })
 
 export default User
