b6b565e91ef871ed58b11586129f6b64ddb404c4
Patrick Boynton <jpboynton3@gmail.com>
2022-10-11 11:13:08 -0400

    Created a product interface.

---
+++ client/src/models/Product.ts
@@ -0,0 +1,14 @@
+interface Product {
+	id: number
+	name: string
+	description: string
+	image: string
+	brand: string
+	category: string
+	price: number
+	countInStock: number
+	rating: number
+	numReviews: number
+}
+
+export default Product
