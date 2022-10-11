9af77084136c71d73edaee037b0596e9a51af890
Patrick Boynton <jpboynton3@gmail.com>
2022-10-11 15:10:27 -0400

    Added routes.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,18 +4,23 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import { Container } from "@mui/material"
 import Home from "./components/Home"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import SingleProduct from "./components/SingleProduct"
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header/>
       <main style={{padding: '20px 0'}}>
         <Container>
-          <Home/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<SingleProduct/>}/>
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { AppBar, Box, Button, Container, IconButton, Toolbar, Typography } from "@mui/material"
 import { faShoppingCart, faUser } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Link } from "react-router-dom"
 
 const Header = () => {
     return  <Box sx={{ flexGrow: 1 }}>
@@ -15,11 +16,19 @@ const Header = () => {
                       sx={{ mr: 2 }}
                     >
                     </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Gametronics
-                    </Typography>
-                    <Button color="inherit"><FontAwesomeIcon icon={faShoppingCart} style={{padding: '20px'}} />Cart</Button>
-                    <Button color="inherit"> <FontAwesomeIcon icon={faUser} style={{padding: '20px'}}/> Login</Button>
+                    <Link to="/">
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            Gametronics
+                        </Typography>
+                    </Link>
+                    <Container>
+                        <Link to="/cart">
+                            <Button  color="inherit"><FontAwesomeIcon icon={faShoppingCart} style={{padding: '20px'}} />Cart</Button>
+                        </Link>
+                        <Link to="/login">
+                            <Button color="inherit"> <FontAwesomeIcon icon={faUser} style={{padding: '20px'}}/> Login</Button>
+                        </Link>
+                    </Container>
                 </Toolbar>
             </Container>
         </AppBar>
