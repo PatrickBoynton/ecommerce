47d1b1c7ddf0394cd1fd953275a9e5da14c13da0
Patrick Boynton <jpboynton3@gmail.com>
2022-10-12 09:39:29 -0400

    Renamed the container.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import { Container } from "@mui/material"
 import Home from "./components/Home"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import SingleProduct from "./components/SingleProduct"
@@ -12,12 +11,10 @@ const App = () => {
     <Router>
       <Header/>
       <main style={{padding: '20px 0'}}>
-        <Container>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/product/:id" element={<SingleProduct/>}/>
+            <Route path="product/:id" element={<SingleProduct/>}/>
           </Routes>
-        </Container>
       </main>
       <Footer />
     </Router>
