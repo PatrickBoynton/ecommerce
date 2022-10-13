4ad0c6a5107d1982c7defc73a5881035d1ec05c9
Patrick Boynton <jpboynton3@gmail.com>
2022-10-13 09:46:31 -0400

    Removed styling. Removed the IconButton.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,21 +1,12 @@
-import { AppBar, Box, Button, Container, IconButton, Toolbar, Typography } from "@mui/material"
+import { AppBar, Box, Button, Container, Toolbar, Typography } from "@mui/material"
 import { faShoppingCart, faUser } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Link } from "react-router-dom"
 
 const Header = () => {
     return  <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" sx={{backgroundColor: "red"}}>
-            <Container>
+        <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                      size="large"
-                      edge="start"
-                      color="inherit"
-                      aria-label="menu"
-                      sx={{ mr: 2 }}
-                    >
-                    </IconButton>
                     <Link to="/">
                         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                             Gametronics
@@ -23,14 +14,13 @@ const Header = () => {
                     </Link>
                     <Container>
                         <Link to="/cart">
-                            <Button  color="inherit"><FontAwesomeIcon icon={faShoppingCart} style={{padding: '20px'}} />Cart</Button>
+                            <Button  color="inherit"><FontAwesomeIcon icon={faShoppingCart} />Cart</Button>
                         </Link>
                         <Link to="/login">
-                            <Button color="inherit"> <FontAwesomeIcon icon={faUser} style={{padding: '20px'}}/> Login</Button>
+                            <Button color="inherit"> <FontAwesomeIcon icon={faUser}/> Login</Button>
                         </Link>
                     </Container>
                 </Toolbar>
-            </Container>
         </AppBar>
     </Box>
 }
