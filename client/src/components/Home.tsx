000d853ad40fc842dabbcc58545fffa24332ed50
Patrick Boynton <jpboynton3@gmail.com>
2022-10-13 09:49:05 -0400

    Changed the products call to the database.

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,16 +1,23 @@
-import products from "../products"
-import { Grid, Typography } from "@mui/material"
+import { Container, Grid, Typography } from "@mui/material"
 import ProductCard from "./ProductCard"
+import { useStore } from "../store/store"
+import { useEffect } from "react"
 
 const Home = () => {
-    return <>
+	const { products, getProducts } = useStore()
+
+	useEffect(() => {
+		getProducts()
+	}, [getProducts])
+
+    return <Container>
 			<Typography variant='h2'>Featured Products</Typography>
 			<Grid container spacing={2}>
-				{products.map(product => <Grid  xs={4} key={product.id} item>
+				{products.map(product => <Grid  xs={3} md={4} sm={12} key={product.id} item>
 						<ProductCard product={product} />
 					</Grid>)}
 			</Grid>
-    </>
+    </Container>
 }
 
 export default Home
