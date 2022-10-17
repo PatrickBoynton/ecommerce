5b72469464ac361c3a7743417c5893c85f93c2bc
Patrick Boynton <jpboynton3@gmail.com>
2022-10-17 15:25:45 -0400

    Created styles in a separate file.

---
+++ client/src/styles/objectStyles.ts
@@ -0,0 +1,20 @@
+const border = "4px solid red"
+
+export const rightPanel = {
+	backgroundColor: "#242424",
+	borderLeft: border,
+	color: "white"
+}
+
+export const detailsGlobal = {
+	borderBottom: border
+}
+
+export const cartButton = {
+	border,
+	color: "white"
+}
+
+export const borderRight = {
+	borderRight: border
+}
