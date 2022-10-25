d715b0f7b6dccd584dd3aea1200e78d0e2df3a0b
Patrick Boynton <jpboynton3@gmail.com>
2022-10-25 09:13:36 -0400

    Added a setCartItems, and getCartItems. Also added a qty state.

--- client/src/store/store-cart.ts
+++ client/src/store/store-cart.ts
@@ -3,15 +3,27 @@ import Product from "../models/Product"
 
 interface StoreCartState {
 	cartItems: Partial<Product[]>
-	setCartItems: (item: Product, qty: number) => void
+	qty: number
+	setCartItems: (item: Product, qty: number, cartItems: Partial<Product[]>) => void,
+	getCartItems: () => void
 }
 
 
 export const useStoreCart = create<StoreCartState>((set) => ({
 	cartItems: [],
-	setCartItems: (item: Product, qty: number) => {
-		set((state) => ({
-			cartItems: [...state.cartItems, item]
-		}))
+	qty: 1,
+	setCartItems: (item: Product, qty: number, cartItems: Partial<Product[]>) => {
+
+		if(item !== undefined && cartItems.indexOf(item) === -1) {
+
+			set((state) => ({
+				cartItems: [item, ...state.cartItems],
+				qty,
+			}))
+		}
+		localStorage.setItem("cart", JSON.stringify(cartItems))
+	},
+	getCartItems: () => {
+		localStorage.getItem("cart")
 	}
 }))
