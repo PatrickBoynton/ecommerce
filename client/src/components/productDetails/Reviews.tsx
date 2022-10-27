f72a96c0c36630ccd18633882a30bb823f3d301a
Patrick Boynton <jpboynton3@gmail.com>
2022-10-27 11:37:45 -0400

    Added the ability to change the rating.

--- client/src/components/productDetails/Reviews.tsx
+++ client/src/components/productDetails/Reviews.tsx
@@ -1,15 +1,21 @@
 import { Rating, Typography } from "@mui/material"
 import { useStoreProducts } from "../../store/store-products"
+import { useState } from "react"
 
 const Reviews = () => {
 	const { product } = useStoreProducts()
-    return <>
+	const [value, setValue] = useState<number | null>(2)
+
+	return <>
 			<Typography variant="h1">Reviews</Typography>
 			<Typography variant="h2">This product is awesome 5 stars!</Typography>
-			<Rating value={product?.rating as number}/>
+			<Rating value={product.rating as number}
+							onChange={(event, newValue) => {
+								setValue(newValue);
+							}}/>
 			<Typography variant="body1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid amet aspernatur expedita labore non officia quibusdam ratione saepe sint.</Typography>
 			<Typography variant="h2">Best gadget ever.</Typography>
-			<Rating  value={product?.rating}/>
+			<Rating  value={product.rating}/>
 			<Typography variant="body1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid amet aspernatur expedita labore non officia quibusdam ratione saepe sint.</Typography>
     </>
 }
