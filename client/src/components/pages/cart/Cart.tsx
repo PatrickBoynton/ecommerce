b8086ce8d308e264d53a9bc6c42becd23994e0f3
Patrick Boynton <jpboynton3@gmail.com>
2022-10-31 11:25:01 -0400

    Initial order layout.

--- client/src/components/pages/cart/Cart.tsx
+++ client/src/components/pages/cart/Cart.tsx
@@ -1,7 +1,9 @@
 import {
+    FormControl,
     Grid,
     List,
-    Typography
+    Typography,
+    Select, MenuItem, Button
 } from "@mui/material"
 import { useEffect, useState } from "react"
 import { useStoreCart } from "../../../store/store-cart"
@@ -46,8 +48,26 @@ const Cart = () => {
             </List> : <Typography variant="h3">No items in your cart. </Typography>}
         </Grid>
         <Grid item xs={4}>
-            <Typography variant="h2">Subtotal: </Typography>
+            <Typography variant="h2">Order Total: </Typography>
             <Typography variant="h3">{cartTotal !== undefined  && cartTotal ? "$" + cartTotal.toFixed(2)  : "$0.00"}</Typography>
+            <Typography variant="h3">Payment Method: </Typography>
+            <FormControl sx={{width: "40%"}}>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  defaultValue="Paypal"
+                  value="Paypal"
+                  label="Payment Method"
+                >
+                    <MenuItem value="Paypal">Paypal</MenuItem>
+                    <MenuItem value="Credit Card">Credit Card</MenuItem>
+                </Select>
+            </FormControl>
+            <Typography variant="h2">Tax: </Typography>
+            <Typography>6%</Typography>
+            <Typography variant="h3">Subtotal: </Typography>
+            <Typography>{(cartTotal * .06 + cartTotal).toFixed(2)}</Typography>
+            <Button sx={{border: "4px solid red", color: "white"}}>Submit Order</Button>
         </Grid>
     </Grid>
 }
