6ef98dd57a3bd421cae1c4ffe653031e13d33e2c
Patrick Boynton <jpboynton3@gmail.com>
2022-11-04 12:46:03 -0400

    Added get all and post endpoints.

--- controllers/cartController.ts
+++ controllers/cartController.ts
@@ -1,9 +1,40 @@
 import { Request, Response } from "express"
+import Cart from "../data/models/Cart"
+import Product from "../data/models/Product"
 
 export const getCart = async (req: Request, res: Response) => {
-	res.send("GET CART")
+	try {
+		const cart = await Cart.findAll()
+
+		return res.send(cart)
+	} catch (e: any) {
+		console.error(e.message)
+		return res.status(500)
+	}
 }
 
 export const postCart = async (req: Request, res: Response) => {
-	res.send("POST CART")
+	const { qty, productId } = req.body
+
+	try {
+		const product = await Product.findByPk(productId)
+		const existingItem = await Cart.findOne({
+			where: {
+				productId,
+			},
+		})
+
+		if (product && !existingItem) {
+			const cartItem = {
+				qty,
+				totalPrice: qty * product.price,
+				productId,
+			}
+			await Cart.create(cartItem)
+			res.status(201).send(cartItem)
+		}
+	} catch (e: any) {
+		console.log(e.message)
+		return res.status(500).send("Something went wrong.")
+	}
 }
