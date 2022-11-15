74b2d278dee762beb3d1a10e09cfea82e52ddb22
Patrick Boynton <jpboynton3@gmail.com>
2022-11-15 08:41:18 -0500

    Added a delete method.

--- client/src/components/pages/cart/CartItemFromDb.tsx
+++ client/src/components/pages/cart/CartItemFromDb.tsx
@@ -3,12 +3,19 @@ import Cart from "../../../models/Cart"
 import { BoxStyle } from "../../../styles/objectStyles"
 import { faTrash } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useStoreCart } from "../../../store/store-cart"
 
 interface Props {
 	cart: Cart
 }
 
 const CartItemFromDb = ({ cart }: Props) => {
+	const { deleteSingleItemDb } = useStoreCart()
+
+	const handleDelete = () => {
+		deleteSingleItemDb(cart.id)
+	}
+
 	return (
 		<ListItem>
 			<Box
@@ -25,10 +32,9 @@ const CartItemFromDb = ({ cart }: Props) => {
 			<FontAwesomeIcon
 				style={{ color: "red", cursor: "pointer" }}
 				icon={faTrash}
-				// onClick={() => {
-				// 	editCartItems(product, cartItems)
-				// 	getStorageItems()
-				// }}
+				onClick={() => {
+					handleDelete()
+				}}
 			/>
 		</ListItem>
 	)
