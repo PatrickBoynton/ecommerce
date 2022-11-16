e9feaf720170451056f8605a5e91f378e4d6ac7f
Patrick Boynton <jpboynton3@gmail.com>
2022-11-16 11:19:52 -0500

    Created some dummy data.

---
+++ data/reviews.ts
@@ -0,0 +1,73 @@
+const reviews = [
+	{
+		name: "Patrick",
+		rating: 5,
+		title:
+			"No, just a regular mistake. We'll need to have a look inside you with this camera.",
+		comment: "Who am I making this out to?",
+		ProductId: 1,
+	},
+	{
+		name: "John",
+		rating: 5,
+		title: "No. We're on the top.",
+		comment:
+			"Oh, but you can. But you may have to metaphorically make a deal with the devil. And by devi, I mean Robot Devil. And by metaphorically, I mean get your coat.",
+		ProductId: 2,
+	},
+	{
+		name: "Jane",
+		rating: 5,
+		title: "Shut up and take my money!",
+		comment: "",
+		ProductId: 1,
+	},
+	{
+		name: "Patrick",
+		rating: 3,
+		title: "There's no part of that sentence I didn't like! ",
+		comment: "Interesting. No, wait, the other thing: tedious. ",
+		ProductId: 4,
+	},
+	{
+		name: "John",
+		rating: 5,
+		title: "Okay, I like a challenge.",
+		comment:
+			"Oh, I don't have time for this. I have to go and buy a single piece of fruit with a coupon and then return it",
+		ProductId: 1,
+	},
+	{
+		name: "Jane",
+		rating: 5,
+		title: "That's not soon enough!",
+		comment: "These old Doomsday Devices are dangerously unstable. ",
+		ProductId: 3,
+	},
+	{
+		name: "Patrick",
+		rating: 5,
+		title: "nd yet you haven't said what I told you to say!",
+		comment:
+			"TThis is the worst part. The calm before the battle. Bender?! You stole the atom.  ",
+		ProductId: 2,
+	},
+	{
+		name: "Jane",
+		rating: 5,
+		title: "That's not soon enough!",
+		comment:
+			"There, now he's trapped in a book I wrote: a crummy world of plot holes and spelling errors!",
+		ProductId: 4,
+	},
+	{
+		name: "Jane",
+		rating: 5,
+		title: "This is the worst part. The calm before the battle",
+		comment:
+			"The alien mother ship is in orbit here. If we can hit that bullseye, the rest of the dominoes will fall like a house of cards. Checkmate.",
+		ProductId: 3,
+	},
+]
+
+export default reviews
