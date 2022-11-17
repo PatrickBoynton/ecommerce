cfab45a9c0c55c415dae910a7b09cb603ed3c362
Patrick Boynton <jpboynton3@gmail.com>
2022-11-17 15:33:46 -0500

    Updated the properties to match the rest of the dummy data.

--- data/reviews.ts
+++ data/reviews.ts
@@ -1,5 +1,6 @@
 const reviews = [
 	{
+		id: 1,
 		name: "Patrick",
 		rating: 5,
 		title:
@@ -8,21 +9,24 @@ const reviews = [
 		ProductId: 1,
 	},
 	{
+		id: 2,
 		name: "John",
-		rating: 5,
+		rating: 4,
 		title: "No. We're on the top.",
 		comment:
 			"Oh, but you can. But you may have to metaphorically make a deal with the devil. And by devi, I mean Robot Devil. And by metaphorically, I mean get your coat.",
 		ProductId: 2,
 	},
 	{
+		id: 3,
 		name: "Jane",
-		rating: 5,
+		rating: 3,
 		title: "Shut up and take my money!",
 		comment: "",
 		ProductId: 1,
 	},
 	{
+		id: 4,
 		name: "Patrick",
 		rating: 3,
 		title: "There's no part of that sentence I didn't like! ",
@@ -30,6 +34,7 @@ const reviews = [
 		ProductId: 4,
 	},
 	{
+		id: 5,
 		name: "John",
 		rating: 5,
 		title: "Okay, I like a challenge.",
@@ -38,35 +43,48 @@ const reviews = [
 		ProductId: 1,
 	},
 	{
+		id: 6,
 		name: "Jane",
-		rating: 5,
+		rating: 2,
 		title: "That's not soon enough!",
 		comment: "These old Doomsday Devices are dangerously unstable. ",
 		ProductId: 3,
 	},
 	{
+		id: 7,
 		name: "Patrick",
-		rating: 5,
-		title: "nd yet you haven't said what I told you to say!",
+		rating: 1,
+		title: "And yet you haven't said what I told you to say!",
 		comment:
 			"TThis is the worst part. The calm before the battle. Bender?! You stole the atom.  ",
 		ProductId: 2,
 	},
 	{
+		id: 8,
 		name: "Jane",
-		rating: 5,
+		rating: 4,
 		title: "That's not soon enough!",
 		comment:
 			"There, now he's trapped in a book I wrote: a crummy world of plot holes and spelling errors!",
 		ProductId: 4,
 	},
 	{
+		id: 9,
 		name: "Jane",
-		rating: 5,
+		rating: 1,
 		title: "This is the worst part. The calm before the battle",
 		comment:
 			"The alien mother ship is in orbit here. If we can hit that bullseye, the rest of the dominoes will fall like a house of cards. Checkmate.",
 		ProductId: 3,
+	},
+	{
+		id: 10,
+		name: "John",
+		rating: 1,
+		title: "I don't know what you did, Fry, but once again, you screwed up!",
+		comment:
+			"When I was first asked to make a film about my nephew, Hubert Farnsworth, I thought Why should I?  Then later, Leela made the film.",
+		ProductId: 5,
 	},
 ]
 
