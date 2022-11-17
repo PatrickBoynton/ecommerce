fd4fcea2550c8e37e3477bec2798596f7f1baf55
Patrick Boynton <jpboynton3@gmail.com>
2022-11-17 15:35:56 -0500

    Refactored the model.

--- data/models/Review.ts
+++ data/models/Review.ts
@@ -1,23 +1,46 @@
 import sequelize from "../../config/sequelize"
-import { DataTypes } from "sequelize"
+import { DataTypes, Model, Optional } from "sequelize"
+import IReview from "../interfaces/IReview"
 
-const Review = sequelize.define("reviews", {
-	name: {
-		type: DataTypes.STRING,
-		allowNull: false,
+type ReviewCreationAttributes = Optional<IReview, "id">
+
+class Review extends Model<IReview, ReviewCreationAttributes> {
+	id: number
+	rating: number
+	title: string
+	comment: string
+}
+
+Review.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		rating: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			defaultValue: 0,
+		},
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		comment: {
+			type: DataTypes.STRING,
+		},
+		ProductId: {
+			type: DataTypes.INTEGER,
+		},
 	},
-	rating: {
-		type: DataTypes.INTEGER,
-		allowNull: false,
-		defaultValue: 0,
-	},
-	title: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-	comment: {
-		type: DataTypes.STRING,
-	},
-})
+	{
+		sequelize,
+		tableName: "review",
+	}
+)
 
 export default Review
